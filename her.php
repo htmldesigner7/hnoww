a48aafa7d947be3ad23bb94189b5fc12933f763d
Yatharth-intelliworkz <htmldesigner4.intelliworkz@gmail.com>
2025-12-09 16:50:48 +0530

    her page add

--- her.php
+++ her.php
@@ -28,7 +28,7 @@
 <section>
     <div class="container">
         <div class="him_wrapper">
-            <div class="him_prod">
+            <a class="him_prod" href="javascript:void(0)">
                 <div class="him_prod_top mb-4">
                     <img class="img-fluid img_1" src="images/home/desire1.png" alt="him_prod">
                     <span class="icon_hert">
@@ -47,32 +47,8 @@
                         <p class>A sanctuary for deep thinking.</p>
                     </div>
                 </div>
-                <div>
-                    <a href="javascript:void(0)" class="com_btn">VIEW OBJECT </a>
-                </div>
-            </div>
-            <div class="him_prod">
-                <div class="him_prod_top mb-4">
-                    <img class="img-fluid img_1" src="images/home/desire1.png" alt="him_prod">
-                    <span class="icon_hert">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="28" viewBox="0 0 30 28" fill="none">
-                            <path
-                                d="M22.1426 1C23.0312 1.00003 23.9132 1.19019 24.7393 1.56152C25.5655 1.93295 26.3221 2.4794 26.9629 3.1748V3.17578C27.6039 3.87135 28.1169 4.70101 28.4678 5.62012C28.8187 6.53946 29 7.52807 29 8.52734C28.9999 9.52647 28.8187 10.5144 28.4678 11.4336C28.1607 12.2379 27.7296 12.9738 27.1973 13.6113L26.9629 13.8789L15 26.8594L3.03711 13.8789C1.74115 12.4726 1.0001 10.5486 1 8.52734C1 6.50595 1.74106 4.58125 3.03711 3.1748C4.33058 1.77138 6.0665 1.00001 7.85742 1C9.64834 1 11.3843 1.7714 12.6777 3.1748L14.2646 4.89648L15 5.69434L15.7354 4.89648L17.3213 3.1748C17.9622 2.47924 18.7195 1.933 19.5459 1.56152C20.3719 1.19024 21.254 1 22.1426 1Z"
-                                stroke="#DF5056" stroke-width="2" />
-                        </svg>
-                    </span>
-                </div>
-                <div>
-                    <div>
-                        <h3 class="sub_head">The Oculus (Solo)</h3>
-                        <p>A compact ritual centerpiece for intimate hosting.</p>
-                    </div>
-                </div>
-                <div>
-                    <a href="javascript:void(0)" class="com_btn">VIEW OBJECT </a>
-                </div>
-            </div>
-            <div class="him_prod">
+            </a>
+            <a class="him_prod" href="javascript:void(0)">
                 <div class="him_prod_top mb-4">
                     <img class="img-fluid img_1" src="images/home/desire1.png" alt="him_prod">
                     <span class="icon_hert">
@@ -88,14 +64,11 @@
                 <div>
                     <div>
                         <h3 class="sub_head">The Pearl Diver’s Ledger</h3>
-                        <p>A sanctuary for deep thinking.</p>
+                        <p class>A sanctuary for deep thinking.</p>
                     </div>
                 </div>
-                <div>
-                    <a href="javascript:void(0)" class="com_btn">VIEW OBJECT </a>
-                </div>
-            </div>
-            <div class="him_prod">
+            </a>
+            <a class="him_prod" href="javascript:void(0)">
                 <div class="him_prod_top mb-4">
                     <img class="img-fluid img_1" src="images/home/desire1.png" alt="him_prod">
                     <span class="icon_hert">
@@ -105,17 +78,36 @@
                                 stroke="#DF5056" stroke-width="2" />
                         </svg>
                     </span>
+
                 </div>
+
                 <div>
                     <div>
-                        <h3 class="sub_head">The Oculus (Solo)</h3>
-                        <p class="my-1">A compact ritual centerpiece for intimate hosting.</p>
+                        <h3 class="sub_head">The Pearl Diver’s Ledger</h3>
+                        <p class>A sanctuary for deep thinking.</p>
                     </div>
                 </div>
-                <div>
-                    <a href="javascript:void(0)" class="com_btn">VIEW OBJECT </a>
+            </a>
+            <a class="him_prod" href="javascript:void(0)">
+                <div class="him_prod_top mb-4">
+                    <img class="img-fluid img_1" src="images/home/desire1.png" alt="him_prod">
+                    <span class="icon_hert">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="28" viewBox="0 0 30 28" fill="none">
+                            <path
+                                d="M22.1426 1C23.0312 1.00003 23.9132 1.19019 24.7393 1.56152C25.5655 1.93295 26.3221 2.4794 26.9629 3.1748V3.17578C27.6039 3.87135 28.1169 4.70101 28.4678 5.62012C28.8187 6.53946 29 7.52807 29 8.52734C28.9999 9.52647 28.8187 10.5144 28.4678 11.4336C28.1607 12.2379 27.7296 12.9738 27.1973 13.6113L26.9629 13.8789L15 26.8594L3.03711 13.8789C1.74115 12.4726 1.0001 10.5486 1 8.52734C1 6.50595 1.74106 4.58125 3.03711 3.1748C4.33058 1.77138 6.0665 1.00001 7.85742 1C9.64834 1 11.3843 1.7714 12.6777 3.1748L14.2646 4.89648L15 5.69434L15.7354 4.89648L17.3213 3.1748C17.9622 2.47924 18.7195 1.933 19.5459 1.56152C20.3719 1.19024 21.254 1 22.1426 1Z"
+                                stroke="#DF5056" stroke-width="2" />
+                        </svg>
+                    </span>
+
                 </div>
-            </div>
+
+                <div>
+                    <div>
+                        <h3 class="sub_head">The Pearl Diver’s Ledger</h3>
+                        <p class>A sanctuary for deep thinking.</p>
+                    </div>
+                </div>
+            </a>
         </div>
     </div>
 </section>
